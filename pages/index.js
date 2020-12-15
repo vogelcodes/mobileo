75f7b91d651fbc809d8b3ff629b25128bf9405f8
Daniel Vogel <danielcvt@gmail.com>
2020-12-15 03:03:10 -0300

    contact

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import {ImWhatsapp} from 'react-icons/im'
 
 export default function Home() {
   return (
@@ -9,31 +10,37 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-      <a
+        <a
             href="https://www.instagram.com/mobi_leo/"
             className={styles.card}
           >
-        <h1 className={styles.title}>
-          mobileo.mobi
-        </h1>
+            <h1 className={styles.title}>
+              mobileo.mobi
+            </h1>
           
-      </a>
+        </a>
 
-        <p className={styles.description}>
-        </p>
+       
         <a
             href="https://www.instagram.com/mobi_leo/">
         
 
-        <img src='https://instagram.fsdu5-1.fna.fbcdn.net/v/t51.2885-15/e35/121235323_1034363926991662_8717067879844545786_n.jpg?_nc_ht=instagram.fsdu5-1.fna.fbcdn.net&_nc_cat=110&_nc_ohc=ynto3FwqmcAAX8OXyEG&tp=1&oh=fd4cfbc929222b4a1a73b120f4d4b061&oe=6000AB9A'></img>
+           <img src='https://instagram.fsdu5-1.fna.fbcdn.net/v/t51.2885-15/e35/121235323_1034363926991662_8717067879844545786_n.jpg?_nc_ht=instagram.fsdu5-1.fna.fbcdn.net&_nc_cat=110&_nc_ohc=ynto3FwqmcAAX8OXyEG&tp=1&oh=fd4cfbc929222b4a1a73b120f4d4b061&oe=6000AB9A'></img>
         </a>
-        
+
+        <ul>
+            <li>Marcenaria personalizada</li>
+            <li>Madeiras maciças, MDF e compensado.</li>
+            <li><ImWhatsapp /> (21)98169-7373</li>
+        </ul>
   
         
       </main>
       <footer className={styles.footer}>
         <h3>
-        mobileo.mobi - 2020 </h3> 
+          mobileo.mobi - 2020
+        </h3>
+        
       </footer>
 
       
